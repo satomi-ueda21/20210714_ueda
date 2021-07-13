19cbdc4915025e918ebf3fc54016d55b1fc50317
satomi-ueda21 <gionkuri@hotmail.com>
2021-07-13 22:35:47 +0900

    first-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Todo from '../views/Todo.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Todo',
+    component:Todo
   }
+  // {
+  //   path: '/home',
+  //   name: 'Home',
+  //   component: Home
+  // }
 ]
 
 const router = new VueRouter({
@@ -25,5 +23,21 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !store.state.auth
+  ) {
+    next({
+      path: "/",
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  } else {
+    next();
+  }
+});
 
 export default router
---
+++ src/views/Todo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="card">
+        <p class="title">Todo List</p>
+        <div class="todo">
+          <div class="input_location">
+            <input type="text" v-model="addText" class="input_add" @keyup.enter="addText()">
+            <button @click="pushAdd" class="button_add">追加</button>
+          </div>
+          <div class="list_display" v-for="(value,index) in lists" :key="index">
+            <input type="text" class="input_apdate" value="{{}}">
+            <!-- <div class="button_set"> -->
+              <button @click="pushUpdate" class="button_update">更新</button>
+              <button @click="pushDelete" class="button_delete">削除</button>
+            <!-- </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      addText:'',
+      lists:[]
+    };
+  },
+  method:{
+    pushAdd(){
+      if(this.addText){
+        this.list.push({text:this.addText});
+      }
+      this.addText='';
+    }
+  }
+};
+</script>
+
+
+<style scoped></style>>
